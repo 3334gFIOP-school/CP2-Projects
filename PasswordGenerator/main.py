d4f82a3e29b9e4acd7201e8f9acbdac1bdc3508b
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-01-13 12:39:17 -0700

    Update main.py

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,15 +1,17 @@
 # Jackson Hauley - Password Generator
 
 import random
+import time
 
 num = 1234567890
 symbol = "!@#$%^&*_-"
 capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
+current_pass = N/A
 
 def main():
     cs()
-    choice = int_input("Password Generator\n\n1. Generate Password\n 2. View Password\n 3. Exit\n\nWhat do you want to do? (1-3): ")
+    choice = int_input(f"Password Generator\nCurrent password: {current_pass}\n\n1. Generate Password\n 2. Test Password\n 3. Exit\n\nWhat do you want to do? (1-3): ")
     if choice == 1:
         cs()
         print("Generate Password")
@@ -22,33 +24,53 @@ def main():
         while True:
             pass_choice = input("Do you want capital letters in your password? (y/n): ")
             if pass_choice == "y":
+                capital = "yes"
                 password = add_capital(password)
                 break
             else: continue
         while True:
             pass_choice = input("Do you want symbols in your password? (y/n): ")
             if pass_choice == "y":
+                symbol = "yes"
                 password = add_symbol(password)
                 break
             else: continue
+        number = "no"
+        symbol = "no"
+        lowercase = "no"
+        capital = "no"
         while True:
             pass_choice = input("Do you want numbers in your password? (y/n): ")
             if pass_choice == "y":
+                number = "yes"
                 password = add_num(password)
                 break
             else: continue
         while True:
             pass_choice = input("Do you want lowercase letters in your password? (y/n): ")
             if pass_choice == "y":
+                lowercase = "yes"
                 password = add_lowercase(password)
                 break
             else: continue
         while "" in password:
             rand = random.randint(1,4)
-            if rand == 1: password = add_lowercase(password)
-            elif rand == 2: password = add_num(password)
-            elif rand == 3: password = add_symbol(password)
-            elif rand == 4: password = add_capital(password)
+            if rand == 1 and lowercase == "yes": password = add_lowercase(password)
+            elif rand == 2 and number == "yes" : password = add_num(password)
+            elif rand == 3: symbol == "yes" = add_symbol(password)
+            elif rand == 4: capital == "yes" = add_capital(password)
+            else: continue
+        current_pass = assemble()
+        print(f"Your generated password was {final_pass}!")
+        print(f"Capitals: {capital}")
+        print(f"Symbols: {symbol}")
+        print(f"Numbers: {number}")
+        print(f"Lowercases: {lowercase}")
+        print(f"Length: {length} characters")
+        input("Press enter to continue")
+        
+        
+        
 
 
 def int_input(text): # Only takes in integers
