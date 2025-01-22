83e3cfb5d9bae814b620752cd7263aa2cfb5d29c
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-01-22 12:47:26 -0700

    eeeeeee

--- MorseCodeTranslator/main.py
+++ MorseCodeTranslator/main.py
@@ -18,7 +18,6 @@ Project needs to:
 # Initializing Lists
 
 alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z') # Alphabet Tuple
-
 morse_alphabet = ( # Morse Code Tuple
     '.-',   # A
     '-...', # B
@@ -63,12 +62,44 @@ def int_input(text): # Only takes in integers
         return output
 
 def main():
+    global english
     while True:
         cs()
-        choice = int_input("Morse code translator\n\n1. English to Morse Code\n2. Morse Code to English\n3. Exit\n\nWhat do you want to do? (1-3): ")
+        choice = int_input("Morse code translator\n\n1. English to Morse Code\n2. Morse Code to English\n3. Exit\n\nWhat do you want to do? (1-3): ") # Seleciton Menu
+        if choice == 1: # English to Morse Code
+            eng_to_mor()
+            print(" | ".join(english)) # Printing it in a readable format
+            input("Press enter to continue")
+
+
+
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            cs()
+            print("Thanks for using Jacksons Morse Code Translator!")
+            exit()
+        else:
+            print("Invalid Input (1-3), try again")
+            input("Press enter to continue")
 
 def eng_to_mor(): # English to morse code
-    pass
+    global english
+    cs()
+    english = input('Type in the sentence you want translated to morse code: ')
+    english.lower()
+    english.split() # Setting up the list
+    for x in english:
+        if x == " ": # Checking for spaces
+            continue
+        for i in alphabet: # Replacing letters with morse code
+            if x == i:
+                english.pop(x)
+                english.insert(x,i)
+
 
 def mor_to_end(): # Morse code to english
     pass
+
+
+main()
