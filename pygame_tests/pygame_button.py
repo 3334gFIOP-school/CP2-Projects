9ca54cf722b04bf11a934f8c60b27094d7511190
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-02-19 13:34:36 -0700

    eee

--- pygame_tests/pygame_button.py
+++ pygame_tests/pygame_button.py
@@ -38,27 +38,13 @@ smallfont = pygame.font.SysFont('Arial',35)
 text = smallfont.render('Quit' , True , white) 
   
 while True: 
-      
-    for ev in pygame.event.get(): 
-          
-        if ev.type == pygame.QUIT: 
-            pygame.quit() 
-              
-        #checks if a mouse is clicked 
-        if ev.type == pygame.MOUSEBUTTONDOWN: 
-              
-            #if the mouse is clicked on the 
-            # button the game is terminated 
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
-                pygame.quit() 
-                  
     # fills the screen with a color 
-    screen.fill((40,40,40)) # R G B
-      
+    screen.fill((40,40,40)) # R G B 
+
     # stores the (x,y) coordinates into 
     # the variable as a tuple 
     mouse = pygame.mouse.get_pos() # Stores mouse coordinates
-      
+
     # if mouse is hovered on a button it 
     # changes to lighter shade  
     if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
@@ -66,9 +52,24 @@ while True:
           
     else: 
         pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
-      
+
     # superimposing the text onto our button 
     screen.blit(text , (width/2+38,height/2)) 
-      
+
     # updates the frames of the game 
     pygame.display.update() 
+
+    for ev in pygame.event.get(): 
+          
+        if ev.type == pygame.QUIT: 
+            pygame.quit() 
+              
+        #checks if a mouse is clicked 
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+              
+            #if the mouse is clicked on the 
+            # button the game is terminated 
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.quit() 
+                  
+    
