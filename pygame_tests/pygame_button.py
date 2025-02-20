eb82216fd8eee91b5a91d5acf1b9ac6182cfc523
3334gFIOP-school <jackson.hauley>
2025-02-19 21:56:37 -0700

    eeeee

--- pygame_tests/pygame_button.py
+++ pygame_tests/pygame_button.py
@@ -1,6 +1,7 @@
 # Jackson Hauley PYGAME TEST
 
 import pygame 
+clock = pygame.time.Clock()
 import sys 
 import random
   
@@ -9,11 +10,11 @@ pygame.init() # initializing the constructor
 # Names the window
 pygame.display.set_caption('Random Title')
 
-# opens the window with the resolution
+# Opens the window with the resolution
 res = (720,720) 
 screen = pygame.display.set_mode(res) 
   
-# colors
+# Colors
 white = (255,255,255) 
 color_light = (170,170,170)
 color_dark = (100,100,100) 
@@ -23,18 +24,23 @@ alphabet = [chr(i) for i in range(97, 123)]
 width = screen.get_width() 
 height = screen.get_height() 
   
-# defining a font 
+# Fonts
 smallfont = pygame.font.SysFont('Arial',35) 
 
-  
-# rendering a text written in 
-# this font 
+# Rendering a text written in the font
+quit_button_text = smallfont.render('Quit' , True , white) 
 
-text = smallfont.render('Quit' , True , white) 
+# COMPLETELY EDITABLE quit location variable
+quit_location = {
+"width" : 140,
+"height" : 40,
+"StartPos": {"x" : 360 ,"y" : 360} # Top left coords
+}
 
-while True: 
-    # fills the screen with a color 
-    screen.fill((40,40,40)) # R G B 
+
+while True: # Main Loop (break after quit)
+
+    screen.fill((40,40,40)) # R G B (fills screen)
 
     # stores the (x,y) coordinates into 
     # the variable as a tuple 
@@ -42,18 +48,15 @@ while True:
 
 
     # QUIT BUTTON ================================
-    if width/4 <= mouse[0] <= width/4+140 and height/4 <= mouse[1] <= height/4+40: 
-        pygame.draw.rect(screen,color_light,[width/4,height/4,140,40]) 
-          
+    if quit_location['StartPos']['x'] <= mouse[0] <= quit_location['StartPos']['x'] + quit_location['width'] and quit_location['StartPos']['y'] <= mouse[1] <= quit_location['StartPos']['y']+quit_location['height']: 
+        pygame.draw.rect(screen,color_light,[quit_location['StartPos']['x'],quit_location['StartPos']['y'],quit_location['width'],quit_location['height']]) # If mouse is hovering
     else: 
-        pygame.draw.rect(screen,color_dark,[width/4,height/4,140,40]) 
+        pygame.draw.rect(screen,color_dark,[quit_location['StartPos']['x'],quit_location['StartPos']['y'],quit_location['width'],quit_location['height']]) # If mouse is not touching
 
     # superimposing the text onto quit button
-    screen.blit(text , (width/4+38,height/4)) 
-    # END QUIT BUTTON ===================================
+    screen.blit(quit_button_text , (quit_location['StartPos']['x']+quit_location['width']/4,quit_location['StartPos']['y'])) 
 
-    # updates the frames of the game 
-    pygame.display.update() 
+
 
     quit = 0
 
@@ -69,12 +72,16 @@ while True:
             # This is where you find out where the button was clicking with the coordinates
             #if the mouse is clicked on the 
             # button the game is terminated 
-            if width/4 <= mouse[0] <= width/4+140 and height/4 <= mouse[1] <= height/4+40: 
+            if quit_location['StartPos']['x'] <= mouse[0] <= quit_location['StartPos']['x']+quit_location['width'] and quit_location['StartPos']['y'] <= mouse[1] <= quit_location['StartPos']['y']+quit_location['height']: 
                 pygame.quit() 
                 quit = 1
                 break
     
     if quit == 1:
-        break
+        break    
 
-    pygame.display.set_caption(random.choice(alphabet))    
+    # updates the frames of the game (always use)
+    clock.tick(60) # Capping at 60fps so my PC doesnt die
+    pygame.display.update() 
+
+        
