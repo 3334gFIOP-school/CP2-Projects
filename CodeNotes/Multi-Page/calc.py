3f0444ae1d3cbd7a926134cdf463b28a4dd0256a
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-02-24 13:20:36 -0700

    eeeee

---
+++ CodeNotes/Multi-Page/calc.py
@@ -0,0 +1,23 @@
+# Jackson Hauley, Multiple Page Python Notes
+num = 10
+
+import time
+
+def addition():
+    return get_num()+get_num()
+
+def subtraction(num,num2):
+    return num-num2
+
+def multiply(num,num2):
+    return num*num2
+
+def divide(num,num2):
+    return num/num2
+
+def exponent(num,num2):
+    return num**num2
+
+def get_num():
+    return int(input("Tell me a number: "))
+
