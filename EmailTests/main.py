707d6d7e9a44c4263c3bac87bf3e29665f2b4e65
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-02-26 12:47:27 -0700

    eeee

--- EmailTests/main.py
+++ EmailTests/main.py
@@ -1,11 +1,12 @@
 import smtplib
 
 # Gmail's SMTP server and port
+
 smtp_server = "smtp.gmail.com"
 smtp_port = 587  # or 465 for SSL
-
 user_email = "jackson.hauley@ucas-edu.net"
-user_password = "set later"  
+user_password = "dont look here"  
+
 
 # Setting up the connection and login
 server = smtplib.SMTP(smtp_server, smtp_port)
@@ -13,14 +14,19 @@ server.starttls()  # Secure the connection using TLS
 server.login(user_email, user_password)
 
 # Send an email (example)
-sender = "your_email@gmail.com"
-recipient = "recipient_email@example.com"
-subject = "Test"
-body = "This is a test email."
 
-message = f"Subject: {subject}\n\n{body}"
+sender = "jackson.hauley@ucas-edu.net"
+recipient = "gavin.pierce@ucas-edu.net"
+subject = "Hello"
+body = "Your mom dot com"
 
-server.sendmail(sender, recipient, message)
+
+y = 1
+for x in range(10):
+    subject = y
+    message = f"Subject: {subject}\n\n{body}"
+    server.sendmail(sender, recipient, message)
+    y += 1
 
 # Close the connection
 server.quit()
