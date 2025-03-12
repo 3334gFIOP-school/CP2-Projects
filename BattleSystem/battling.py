480cfd188c519c9e276b1082950d6ea9a31f5299
3334gFIOP-school <jackson.hauley>
2025-03-12 11:48:44 -0600

    eeeeee

--- BattleSystem/battling.py
+++ BattleSystem/battling.py
@@ -1,4 +1,26 @@
 # Battling functions
+from characters import *
 
 def battle():
-    pass
+    character1 = input("Name of defending character: ")
+    character2 = input("Name of attacking character: ")
+    while True:
+        if character1.lower().strip() == character2.lower().strip():
+            print("Characters cannot be the same!")
+            character2 = input("Name of attacking character: ")
+        else:
+            break
+    for x in characters:
+        if x["name"].lower().strip() == character1.lower().strip():
+            character1 = x
+            break
+    for x in characters:
+        if x["name"].lower().strip() == character2.lower().strip():
+            character2 = x
+            break
+    running = True
+    def round():
+            cs()
+            print(f"{character1["name"]}")
+    while running:
+        round()
